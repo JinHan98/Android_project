07e11950141303ff62bf08a23d2aa19e8d829ceb
dongho <chaewon1130@naver.com>
2019-12-04 18:55:08 +0900

    myinfo Activity 추가

--- app/src/main/java/com/example/androidproject_maps/MypageActivity.kt
+++ app/src/main/java/com/example/androidproject_maps/MypageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidproject_maps
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,11 +13,13 @@ class MypageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_mypage)
 
         reviewbt.setOnClickListener{
-            Toast.makeText(this,"업데이트 예정입니다.", Toast.LENGTH_SHORT).show()
+            val reviewintent = Intent(this, ReviewActivity::class.java)
+            startActivity(reviewintent)
         }
 
         myinfobt.setOnClickListener{
-            Toast.makeText(this,"업데이트 예정입니다.", Toast.LENGTH_SHORT).show()
+            val myinfointent = Intent(this, MyinfoActivity::class.java)
+            startActivity(myinfointent)
         }
 
         logoutbt.setOnClickListener{
