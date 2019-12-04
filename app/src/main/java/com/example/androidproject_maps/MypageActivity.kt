bb002392619b3393ea84c2120bff330be9b75137
dongho <chaewon1130@naver.com>
2019-12-04 17:19:34 +0900

    mypage 수정, logindetailActivity 추가

--- app/src/main/java/com/example/androidproject_maps/MypageActivity.kt
+++ app/src/main/java/com/example/androidproject_maps/MypageActivity.kt
@@ -1,12 +1,31 @@
 package com.example.androidproject_maps
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_mypage.*
 
 class MypageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
+
+        reviewbt.setOnClickListener{
+            Toast.makeText(this,"업데이트 예정입니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        myinfobt.setOnClickListener{
+            Toast.makeText(this,"업데이트 예정입니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        logoutbt.setOnClickListener{
+            Toast.makeText(this,"업데이트 예정입니다.", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
+
+
+
 }
